9961ef590a7b3c1f0e760806b85698effeaa9fd5
FALODUN EBENEZER <olatunbosunfalodun@yahoo.com>
2023-08-14 06:27:19 -0700

    Initial commit

---
+++ src/main/java/com/ebenzlab/springcloudecommerceproject/SpringCloudEcommerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ebenzlab.springcloudecommerceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEcommerceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEcommerceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebenzlab/springcloudecommerceproject/SpringCloudEcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebenzlab.springcloudecommerceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEcommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
