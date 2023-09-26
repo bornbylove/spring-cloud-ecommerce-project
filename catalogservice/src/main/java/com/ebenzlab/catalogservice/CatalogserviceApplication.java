a9d2d966f928b911ee9a61ac637a140a1611c1d9
FALODUN EBENEZER <olatunbosunfalodun@yahoo.com>
2023-09-25 17:20:33 -0700

    CatlogService and RegistryService Updated

--- catalogservice/src/main/java/com/ebenzlab/catalogservice/CatalogserviceApplication.java
+++ catalogservice/src/main/java/com/ebenzlab/catalogservice/CatalogserviceApplication.java
@@ -3,16 +3,20 @@ package com.ebenzlab.catalogservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @EnableFeignClients
 @EnableCircuitBreaker
-@EnableAutoConfiguration
+@EnableEurekaClient
 @SpringBootApplication
 public class CatalogserviceApplication {
 	@Bean
@@ -20,6 +24,8 @@ public class CatalogserviceApplication {
 	public RestTemplate restTemplate(){
 		return new RestTemplate();
 	}
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogserviceApplication.class, args);
